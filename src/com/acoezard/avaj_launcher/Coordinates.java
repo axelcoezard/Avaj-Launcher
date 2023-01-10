dd213d3b4ae84852fb86f3c2f154100f6c3a7c14
axelcoezard <axel.coezard@gmail.com>
2023-01-10 08:48:27 +0100

    coordinates

---
+++ src/com/acoezard/avaj_launcher/Coordinates.java
@@ -0,0 +1,33 @@
+package com.acoezard.avaj_launcher;
+
+
+class Coordinates {
+    private int longitude;
+    
+    private int latitude;
+
+    private int height;
+
+    public Coordinates(int longitude, int latitude, int height) {
+        this.longitude = longitude;
+        this.latitude = latitude;
+        this.height = height;
+    }
+
+    public int getLongitude() {
+        return this.longitude;
+    }
+
+    public int getLatitude() {
+        return this.latitude;
+    }
+
+    public int getHeight() {
+        return this.height;
+    }
+
+    @Override
+    public String toString() {
+        return this.longitude + "," + this.latitude + "," + this.height;
+    }
+}
