193188628fab6f2f02e7fe4f65cc17e5c335e44d
axelcoezard <axel.coezard@gmail.com>
2023-01-11 09:51:53 +0100

    logger begin

--- src/com/acoezard/avaj_launcher/utils/Logger.java
+++ src/com/acoezard/avaj_launcher/utils/Logger.java
@@ -1,5 +1,22 @@
 package com.acoezard.avaj_launcher.utils;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Logger {
-    
+    private static BufferedWriter bufferedWriter;
+
+    public Logger(String filename) throws IOException {
+        if (bufferedWriter == null) {
+            bufferedWriter = new BufferedWriter(new FileWriter(filename));
+        }
+    }
+
+    public static void log(String log) throws IOException {
+        if (bufferedWriter == null) {
+            throw new IOException("No logger instance");
+        }
+        bufferedWriter.append(log);
+    }
 }
